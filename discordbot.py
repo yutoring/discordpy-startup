bc3f064f1cc4f7605104ca165efc42e46a86061a
yutoring <57298548+yutoring@users.noreply.github.com>
2019-11-16 16:51:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,17 +1,39 @@
 from discord.ext import commands
 import os
-import datetime
+from datetime import datetime
 import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+dateTimeList = [
+'17:00'
+]
 
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
 
 
+@tasks.loop(seconds=30)
+async def time_check():
+    sleepTime = 0
+    # 現在の時刻
+    now = datetime.now().strftime('%Y/%m/%d %H:%M')
+    if now in dateTimeList :
+        print(now)
+        await SendMessage('asd')
+        #該当時間だった場合は２重に投稿しないよう３０秒余計に待機
+        await asyncio.sleep(30)
+
+
+
+@Client.event
+
+async def time_check
+await SendMessage('asdasd')
+ 
+    
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
