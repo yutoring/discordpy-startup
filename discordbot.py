53cf3cd62f4c38c53ad44891c68f3969bfa5e633
yutoring <57298548+yutoring@users.noreply.github.com>
2019-11-16 18:02:10 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,29 +8,59 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 target_channel_id = 325988737572012033
 
-@bot.event
-async def on_command_error(ctx, error):
-    await ctx.send(str(error))
-    
-@tasks.loop(hours=24)
-async def called_once_a_day():
-    message_channel = bot.get_channel(target_channel_id)
-    print(f"Got channel {message_channel}")
-    await ctx.send("Test man is testing")
-
-@called_once_a_day.before_loop
-async def before():
-    await bot.wait_until_ready()
-    print("Eyyy B0sss!")
+client = discord.Client()
 
 
+#投稿する日時
+dateTimeList = [
+'2019/11/16 18:09',
+'2019/05/20 18:30',
+'2019/05/21 18:30',
+'2019/05/22 07:00',
+'2019/05/23 07:00',
+'2019/05/24 07:00',
+'2019/05/25 07:00'
+]
 
-    
-@bot.command()
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    print('ready')
+
+# 指定時間に走る処理
+async def SendMessage():
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send('時間だよ')
+
+# 30秒に一回ループ
+@tasks.loop(seconds=30)
+async def time_check():
+    sleepTime = 0
+    # 現在の時刻
+    now = datetime.now().strftime('%Y/%m/%d %H:%M')
+    if now in dateTimeList :
+        print(now)
+        await SendMessage()
+        #該当時間だった場合は２重に投稿しないよう３０秒余計に待機
+        await asyncio.sleep(30)
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 使用できるコマンド一覧
+    if message.content == '!help':
+        await message.channel.send('現在使用できるコマンドはありません')
+        
+        @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
 
-
-called_once_a_day.start()
+#ループ処理
+time_check.start()
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
 bot.run(token)
